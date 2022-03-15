f1431cda01c543439db064c0cad59b82297c38bd
bajsi <lbaisinger@outlook.com>
2022-03-15 23:16:41 +0100

    added dashboard and basic html structure

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from weather_module import *
 
-app = Flask('FullCalendar Demo')
+app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    # return render_template('index.html')
-    return "Home"
+    return render_template('home.html')
 
 
-@app.route('/dashboard')
+@app.route('/dashboard', methods=['POST'])
 def dashboard():
-    weather_data = get_weather_data('01219', 'de', get_api_key())
-    # return render_template('calendar.html')
+    zip_code = request.form['zipCode']
+    country_code = request.form['countryCode']
+    api_key = get_api_key()
+    weather_data = get_weather_data(zip_code, country_code, api_key)
+    temperature = "{0:.2f}".format(weather_data['main']['temp'])
+    temperature_feels_like = '{0:.2f}'.format(weather_data['main']['feels_like'])
+    # todo add icon
+    return render_template('dashboard.html',
+                           temperature=temperature,
+                           temperature_feels_like=temperature_feels_like)
 
 
 if __name__ == '__main__':
