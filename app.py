8e72d79bd3b981396c2b76b429fe694455d3edc1
bajsi <lbaisinger@outlook.com>
2022-07-03 18:35:43 +0200

    bootstrap with temperature and calendar framework is done

--- app.py
+++ app.py
@@ -9,6 +9,21 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/bootstrap', methods=['POST'])
+def bootstrap():
+    zip_code = request.form['zipCode']
+    country_code = request.form['countryCode']
+    api_key = get_api_key()
+    weather_data = get_weather_data(zip_code, country_code, api_key)
+    temperature_kelvin = "{0:.2f}".format(weather_data['main']['temp'])
+    temperature_feels_like_kelvin = '{0:.2f}'.format(weather_data['main']['feels_like'])
+    temperature = float(temperature_kelvin) - 273.15
+    temperature_feels_like = float(temperature_feels_like_kelvin) - 273.15
+    return render_template('bootstrap.html',
+                           temperature="{:.2f}".format(temperature),
+                           temperature_feels_like="{:.2f}".format(temperature_feels_like))
+
+
 @app.route('/dashboard', methods=['POST'])
 def dashboard():
     zip_code = request.form['zipCode']
@@ -19,7 +34,6 @@ def dashboard():
     temperature_feels_like_kelvin = '{0:.2f}'.format(weather_data['main']['feels_like'])
     temperature = float(temperature_kelvin) - 273.15
     temperature_feels_like = float(temperature_feels_like_kelvin) - 273.15
-    # todo add icon
     return render_template('dashboard.html',
                            temperature="{:.2f}".format(temperature),
                            temperature_feels_like="{:.2f}".format(temperature_feels_like))
