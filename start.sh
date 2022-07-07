737d4dc824f479c5c3602527dfc7b27348c480d7
bajsi <lbaisinger@outlook.com>
2022-07-07 21:34:53 +0200

    readme update

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+unclutter &
+xset -dpms # disable DPMS (Energy Star) features.
+xset s off # disable screen saver
+xset s noblank # don’t blank the video device
+matchbox-window-manager &
+chromium-browser –-incognito –-kiosk http://http://192.168.2.105:5000//
