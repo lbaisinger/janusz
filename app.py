97238cc5f1b7b2d951c35826d4a71fbe94769c5b
bajsi <lbaisinger@outlook.com>
2022-07-07 20:36:12 +0200

    version 0.1 ready Framework Time, local temperature and calendar with bootstrap.

--- app.py
+++ app.py
@@ -24,20 +24,6 @@ def bootstrap():
                            temperature_feels_like="{:.2f}".format(temperature_feels_like))
 
 
-@app.route('/dashboard', methods=['POST'])
-def dashboard():
-    zip_code = request.form['zipCode']
-    country_code = request.form['countryCode']
-    api_key = get_api_key()
-    weather_data = get_weather_data(zip_code, country_code, api_key)
-    temperature_kelvin = "{0:.2f}".format(weather_data['main']['temp'])
-    temperature_feels_like_kelvin = '{0:.2f}'.format(weather_data['main']['feels_like'])
-    temperature = float(temperature_kelvin) - 273.15
-    temperature_feels_like = float(temperature_feels_like_kelvin) - 273.15
-    return render_template('dashboard.html',
-                           temperature="{:.2f}".format(temperature),
-                           temperature_feels_like="{:.2f}".format(temperature_feels_like))
-
 
 if __name__ == '__main__':
     app.run()
