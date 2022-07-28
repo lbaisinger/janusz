f0026837c4035ee032617b78b105d89ee8048b01
bajsi <lbaisinger@outlook.com>
2022-07-28 21:29:56 +0200

    row height finally constant!

--- app.py
+++ app.py
@@ -5,22 +5,20 @@ import os
 app = Flask(__name__)
 
 
-# @app.route('/')
-# def home():
-#     return render_template('home.html')
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 
 @app.route('/base')
 def base():
     return render_template('base.html')
 
-# , methods=['POST']
-@app.route('/')
+
+@app.route('/bootstrap', methods=['POST'])
 def bootstrap():
-    # zip_code = request.form['zipCode']
-    zip_code = '01219'
-    # country_code = request.form['countryCode']
-    country_code = 'de'
+    zip_code = request.form['zipCode']
+    country_code = request.form['countryCode']
     api_key = get_api_key()
     weather_data = get_weather_data(zip_code, country_code, api_key)
     temperature_kelvin = "{0:.2f}".format(weather_data['main']['temp'])
@@ -28,7 +26,8 @@ def bootstrap():
     temperature = float(temperature_kelvin) - 273.15
     temperature_feels_like = float(temperature_feels_like_kelvin) - 273.15
     return render_template('bootstrap.html',
-                           temperature="{:.1f}".format(temperature))
+                           temperature="{:.2f}".format(temperature),
+                           temperature_feels_like="{:.2f}".format(temperature_feels_like))
 
 
 if __name__ == "__main__":
